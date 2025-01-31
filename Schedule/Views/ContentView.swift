159d653932a37a322b4c0db0d0eb2d5d95dc990a
JDeWeese <jdeweese574@icloud.com>
2025-01-31 07:34:10 -0600

    custom tab bar complete

--- Schedule/Views/ContentView.swift
+++ Schedule/Views/ContentView.swift
@@ -26,6 +26,7 @@ struct ContentView: View {
                         .fontDesign(.serif)
                         .background {
                             let progress = min(max((scrollOffset + startTopInset - 110) / 15, 0), 1)
+                            
                             ZStack(alignment: .bottom) {
                                 Rectangle()
                                     .fill(.ultraThinMaterial)
@@ -43,8 +44,9 @@ struct ContentView: View {
                     
                     /// YOUR OTHER VIEW HERE
                     LazyVStack(alignment: .leading) {
-                        Text(activeTab.rawValue + (activeTab == .all ? "" : " Mails"))
-                            .font(.caption2)
+                        Text(activeTab.rawValue + (activeTab == .all ? "" : " Objectives"))
+                            .fontDesign(.serif)
+                            .font(.caption)
                             .foregroundStyle(.gray)
                             .frame(maxWidth: .infinity, alignment: .leading)
                     }
@@ -52,49 +54,130 @@ struct ContentView: View {
                     .zIndex(0)
                 }
             }
-                .onScrollGeometryChange(for: CGFloat.self, of: {
-                    $0.contentOffset.y
-                }, action: { oldValue, newValue in
-                    scrollOffset = newValue
-                })
-                .onScrollGeometryChange(for: CGFloat.self, of: {
-                    $0.contentInsets.top
-                }, action: { oldValue, newValue in
-                    if startTopInset == .zero {
-                        startTopInset = newValue
+            .onScrollGeometryChange(for: CGFloat.self, of: {
+                $0.contentOffset.y
+            }, action: { oldValue, newValue in
+                scrollOffset = newValue
+            })
+            .onScrollGeometryChange(for: CGFloat.self, of: {
+                $0.contentInsets.top
+            }, action: { oldValue, newValue in
+                if startTopInset == .zero {
+                    startTopInset = newValue
+                }
+                topInset = newValue
+            })
+            .onScrollGeometryChange(for: CGFloat.self, of: {
+                $0.contentInsets.top
+            }, action: { oldValue, newValue in
+                if startTopInset == .zero {
+                    startTopInset = newValue
+                }
+                topInset = newValue
+            })
+            .background(.gray.opacity(0.1))
+            //MARK:  ToolBar
+            .toolbar {
+                ToolbarItem(placement: .topBarLeading) {
+                    EditButton()
+                }
+                ToolbarItem (placement: .principal) {
+                    LogoView()
+                }
+                ToolbarItem(placement: .topBarTrailing){
+                    Button{
+                        showAddSheet = true
+                        //         HapticManager.notification(type: .success)
+                    } label: {
+                        Image(systemName: "plus")
+                            .font(.title3)
+                            .fontWeight(.semibold)
+                            .foregroundStyle(.white)
+                            .frame(width: 45, height: 45)
+                            .background(.blue.gradient, in: .circle)
+                            .contentShape(.circle)
                     }
-                    topInset = newValue
-                })
-                //MARK:  ToolBar
-                .toolbar {
-                    ToolbarItem(placement: .topBarLeading) {
-                        EditButton()
+                    .sheet(isPresented: $showAddSheet) {
+                        AddItemView()
+                            .presentationDetents([.medium])
                     }
-                    ToolbarItem (placement: .principal) {
-                        LogoView()
-                    }
-                    ToolbarItem(placement: .topBarTrailing){
-                        Button{
-                            showAddSheet = true
-                            //         HapticManager.notification(type: .success)
-                        } label: {
-                            Image(systemName: "plus")
-                                .font(.title3)
-                                .fontWeight(.semibold)
-                                .foregroundStyle(.white)
-                                .frame(width: 45, height: 45)
-                                .background(.blue.gradient, in: .circle)
-                                .contentShape(.circle)
+                    .blur(radius: showAddSheet ? 8 : 0)
+                }
+            }
+            
+        }
+    }
+}
+    struct CustomTabBar: View {
+        @Binding var activeTab: TabModel
+        @Environment(\.colorScheme) private var scheme
+        var body: some View {
+            GeometryReader {
+                let size = $0.size
+                let allOffset = size.width - (40 * CGFloat(TabModel.allCases.count - 1))
+                
+                HStack(spacing: 8) {
+                    HStack(spacing: activeTab == .all ? -15 : 8) {
+                        ForEach(TabModel.allCases.filter({ $0 != .all }), id: \.rawValue) { tab in
+                            ResizableTab(tab)
                         }
-                        .sheet(isPresented: $showAddSheet) {
-                            AddItemView()
-                                .presentationDetents([.medium])
-                        }
-                        .blur(radius: showAddSheet ? 8 : 0)
+                    }
+                    
+                    if activeTab == .all {
+                        ResizableTab(.all)
+                            .transition(.offset(x: allOffset))
                     }
                 }
-              
+                .padding(.horizontal, 15)
+            }
+            .frame(height: 50)
         }
+       
+    
+    @ViewBuilder
+    func ResizableTab(_ tab: TabModel) -> some View {
+        
+                HStack(spacing: 8) {
+                    Image(systemName: tab.symbolImage)
+                        .opacity(activeTab != tab ? 1 : 0)
+                        .overlay {
+                            Image(systemName: tab.symbolImage)
+                                .symbolVariant(.fill)
+                                .opacity(activeTab == tab ? 1 : 0)
+                        }
+                    
+                    if activeTab == tab {
+                        Text(tab.rawValue)
+                            .font(.callout)
+                            .fontWeight(.semibold)
+                            .lineLimit(1)
+                    }
+                }
+                .foregroundStyle(tab == .all ? .gray : activeTab == tab ? .white : .gray)
+                .frame(maxHeight: .infinity)
+                .frame(maxWidth: activeTab == tab ? .infinity : nil)
+                .padding(.horizontal, activeTab == tab ? 10 : 20)
+                .background {
+                    Rectangle()
+                        .fill(activeTab == tab ? tab.color : .inActiveTab)
+                }
+                .clipShape(.rect(cornerRadius: 20, style: .continuous))
+                .background {
+                    RoundedRectangle(cornerRadius: 20, style: .continuous)
+                        .fill(.background)
+                        .padding(activeTab == .all && tab != .all ? -3 : 3)
+                }
+                .contentShape(.rect)
+                .onTapGesture {
+                    guard tab != .all else { return }
+                    withAnimation(.bouncy) {
+                        if activeTab == tab {
+                            activeTab = .all
+                        } else {
+                            activeTab = tab
+                        }
+                    }
+                }
     }
 }
 #Preview {
